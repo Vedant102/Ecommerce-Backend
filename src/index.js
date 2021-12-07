058cd94776e5308b792563d1aab1c17ca371e792
Vedant102 <jadhavvedant102@gmail.com>
2021-12-07 15:39:48 +0530

    Auth and Middleware

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import connectDB from './services/mongoDB/connectDB'
-dotenv.config() 
+import connectDB from './services/mongodb/connectDB'
+dotenv.config()
+import cors from 'cors'
+import authRoutes from './routes/authRoutes'
 
 const app = express()
 const port = process.env.PORT || 3003
 
 connectDB()
 
-app.listen(port,(req, res) =>{
-  console.log(`Server Listning at PORT ${port}`)
+app.use(cors())
+app.use(express.json())
+
+//route to handle auth requests
+app.use("/api/v1/auth", authRoutes)
+
+
+app.listen(port, (req, res) => {
+    console.log(`Server listening at PORT ${port}`)
 })
